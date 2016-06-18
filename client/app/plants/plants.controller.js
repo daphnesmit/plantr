0a9519efeab53fbe029e16b16775acfd51190f23
Daphne Smit <Daphne@tamtam.nl>
2016-06-18 18:21:36 +0200

    Plant route added

---
+++ client/app/plants/plants.controller.js
@@ -0,0 +1,43 @@
+'use strict';
+(function(){
+
+class PlantsController {
+  constructor($http, $scope, socket) {
+    this.$http = $http;
+    this.socket = socket;
+    this.awesomeThings = [];
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('thing');
+    });
+  }
+
+  $onInit() {
+    this.$http.get('/api/things')
+            .then(response => {
+      this.awesomeThings = response.data;
+    this.socket.syncUpdates('thing', this.awesomeThings);
+  });
+}
+
+  addThing() {
+    if (this.newThing) {
+      this.$http.post('/api/things', {
+        name: this.newThing
+      });
+      this.newThing = '';
+    }
+  }
+
+  deleteThing(thing) {
+    this.$http.delete('/api/things/' + thing._id);
+  }
+}
+
+angular.module('plantrApp')
+  .component('plants', {
+    templateUrl: 'app/plants/plants.html',
+    controller: PlantsController
+  });
+
+})();
---
+++ client/app/plants/plants.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('plantrApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('plants', {
+        url: '/plants',
+        template: '<plants></plants>'
+      });
+  });
